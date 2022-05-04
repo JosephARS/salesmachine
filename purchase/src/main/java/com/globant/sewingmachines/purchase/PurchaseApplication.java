01ef1578f7727fe4c5a7c0d27b6eaeb3868560b9
Joseph <joseph.rodriguez@globant.com>
2022-05-04 10:35:13 -0500

    Change DB local to AWS

--- purchase/src/main/java/com/globant/sewingmachines/purchase/PurchaseApplication.java
+++ purchase/src/main/java/com/globant/sewingmachines/purchase/PurchaseApplication.java
@@ -2,8 +2,10 @@ package com.globant.sewingmachines.purchase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PurchaseApplication {
 
 	public static void main(String[] args) {
