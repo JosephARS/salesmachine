8c44d21774646fbb4dad954f7369cbba7e87150c
Joseph <joseph.rodriguez@globant.com>
2022-05-04 10:46:48 -0500

    Gateway service fix

--- gateway-service/src/main/java/com/globant/sewingmachines/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/globant/sewingmachines/gatewayservice/GatewayServiceApplication.java
@@ -2,7 +2,9 @@ package com.globant.sewingmachines.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
 
