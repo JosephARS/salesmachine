fc6c88edb96ba386caca1fc758d04d71a3a76c3e
Joseph <joseph.rodriguez@globant.com>
2022-05-04 09:00:08 -0500

    registry-service creation

---
+++ registry-service/src/test/java/com/globant/sewingmachines/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.sewingmachines.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
