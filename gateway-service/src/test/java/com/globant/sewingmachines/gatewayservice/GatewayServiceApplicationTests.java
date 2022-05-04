7a444c2f0531676322273401a2295ffd8014b491
Joseph <joseph.rodriguez@globant.com>
2022-05-04 09:00:51 -0500

    gateway-service creation

---
+++ gateway-service/src/test/java/com/globant/sewingmachines/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.sewingmachines.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
